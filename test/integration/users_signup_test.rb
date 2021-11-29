f2f4a825ff632f69ebee120e92bc11a80572f077
John Berry <jonathanberry1218@gmail.com>
2021-11-29 09:53:13 -0600

    Add account activation

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup information" do
     get signup_path
     user = {
@@ -17,7 +21,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.field_with_errors'
   end
 
-  test "valid signup" do
+  test "valid signup information with account_activation" do
     get signup_path
     user = {
       name: "John Doe",
@@ -28,9 +32,22 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_difference 'User.count', 1 do
       post users_path, params: { user: user }
     end
-    follow_redirect!
-    assert_template 'users/show'
-    assert_not flash.empty?
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    # Try to log in before activation
+    log_in_as(user)
+    assert_not is_logged_in?
+    # Invalid activation token
+    get edit_account_activation_path("Invalid token", email: user.email)
+    assert_not is_logged_in?
+    # Valid token, wrong email
+    get edit_account_activation_path(user.activation_token, email: "wrong")
+    assert_not is_logged_in?
+    # Valid token and email
+    get edit_account_activation_path(user.activation_token, email: user.email)
     assert is_logged_in?
+    assert user.reload.activated?
+    follow_redirect!
+    assert_template "users/show"
   end
 end
